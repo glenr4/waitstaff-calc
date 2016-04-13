59fe9cf4bb8a1672c562f9e12714ca6dcf9cee9a
glenr4 <glenrutherford4@gmail.com>
2016-04-13 17:30:32 +1000

    Routes and data binding complete

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+// Notes from the course: "In this refactor, you will have to use $rootScope. While this is not a good way to build production code, it's important to learn these concepts before you move into handling data. Later in the course, you will learn about factories and services, which would be the ideal way to handle this data."
+
 angular.module('waitStaffApp', ['ngMessages', 'ngRoute'])
 	.config(['$routeProvider', function($routeProvider){
 		$routeProvider
@@ -20,13 +22,13 @@ angular.module('waitStaffApp', ['ngMessages', 'ngRoute'])
 		})
 		.otherwise('/error');
 	}])
-	.controller('waitStaffCtrl', function($scope, $timeout){
-
-		var mealData = [];
-
+	.controller('HomeCtrl', function($scope){
+		//
+	})
+	.controller('MealCtrl', function($scope, $rootScope, $timeout){
 		// Submit only occurs if the form is valid
 		$scope.submit = function(){
-			mealData.push({
+			$rootScope.mealData.push({
 				"mealPrice": $scope.mealPrice, 
 				"taxRate": $scope.taxRate, 
 				"tipPcnt": $scope.tipPcnt});
@@ -34,11 +36,49 @@ angular.module('waitStaffApp', ['ngMessages', 'ngRoute'])
 			// Call other functions
 			clearForm();
 			customerCharges();
-			earningsInfo();
 		};
 
 		$scope.cancel = function(){
 			clearForm();
+		};
+
+		customerCharges = function(){
+			var thisMeal = $rootScope.mealData[$rootScope.mealData.length - 1];
+			$scope.custSubTotal = thisMeal.mealPrice * (1 + thisMeal.taxRate/100);
+
+			$scope.custTip = thisMeal.mealPrice * thisMeal.tipPcnt/100;
+			$scope.custTotal = $scope.custSubTotal + $scope.custTip;
+		};
+
+		clearForm = function(){
+			$scope.mealPrice = "";
+			$scope.taxRate = "";
+			$scope.tipPcnt = "";
+
+			$scope.detailsForm.$setPristine();
+			$scope.detailsForm.$setUntouched();
+		};
+
+		// Initialise form
+		$scope.init= function(){
+			$timeout(function() {
+				clearForm();
+
+				$scope.custSubTotal = 0;
+				$scope.custTip = 0;
+				$scope.custTotal = 0;				
+			}, 0);
+		};
+	})
+	.controller('EarningsCtrl', function($scope, $rootScope){
+		earningsInfo = function(){
+			$scope.earningsTip = 0;
+			for (var i=0; i < $rootScope.mealData.length; i++){
+				$scope.earningsTip = $scope.earningsTip + $rootScope.mealData[i].mealPrice * $rootScope.mealData[i].tipPcnt/100;
+			};
+
+			$scope.earningsCount = $rootScope.mealData.length;
+			$scope.earningsAvgTip = $scope.earningsTip/$scope.earningsCount;
 		};
 
 		$scope.reset = function(){
@@ -50,50 +90,12 @@ angular.module('waitStaffApp', ['ngMessages', 'ngRoute'])
 			$scope.earningsCount = "0";
 			$scope.earningsAvgTip = "0";
 
-			clearForm();
+			$rootScope.mealData.length = 0;
 		};
 
-		customerCharges = function(){
-			var thisMeal = mealData[mealData.length - 1];
-			$scope.custSubTotal = thisMeal.mealPrice * (1 + thisMeal.taxRate/100);
-
-			$scope.custTip = thisMeal.mealPrice * thisMeal.tipPcnt/100;
-			$scope.custTotal = $scope.custSubTotal + $scope.custTip;
-		};
-
-		earningsInfo = function(){
-			$scope.earningsTip = 0;
-			for (var i=0; i < mealData.length; i++){
-				$scope.earningsTip = $scope.earningsTip + mealData[i].mealPrice * mealData[i].tipPcnt/100;
-			};
-
-			$scope.earningsCount = mealData.length;
-			$scope.earningsAvgTip = $scope.earningsTip/$scope.earningsCount;
-		};
-
-		clearForm = function(){
-			// Clear form
-			$scope.mealPrice = "";
-			$scope.taxRate = "";
-			$scope.tipPcnt = "";
-
-			$scope.detailsForm.$setPristine();
-			$scope.detailsForm.$setUntouched();
-		};
-
-		// Initialise Customer Charges and Earnings
-		$scope.init = function(){
-			$timeout(function() {
-				$scope.reset();
-			}, 0);
-		};
+		// Update earnings
+		earningsInfo();
 	})
-	.controller('HomeCtrl', function($scope){
-
-	})
-	.controller('MealCtrl', function($scope){
-
-	})
-	.controller('EarningsCtrl', function($scope){
-
+	.run(function($rootScope, $timeout){
+		$rootScope.mealData = [];	
 	});
